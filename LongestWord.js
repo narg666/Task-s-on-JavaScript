3aa420a72f336182189ab73958bb3a84804b1e48
Valera Lyubavskiy <javanarg666@gmail.com>
2015-11-16 03:05:08 +0300

    Add Longest word in string

---
+++ LongestWord.js
@@ -0,0 +1,17 @@
+// Нахождение самого длинного слова в строке.
+
+function findLongestWord(str) {
+  var words = str.split(' ');
+  var longest = 0;
+
+  for (var i=0;i<words.length;i++) {
+    if (words[i].length > longest) {
+      longest = words[i].length;
+    }
+
+    
+  }
+  return longest;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
