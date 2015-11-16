c666c84f016047d45285bc6dfd7ba717d50b7284
Valera Lyubavskiy <javanarg666@gmail.com>
2015-11-16 03:13:09 +0300

    Add largestNum in array task

---
+++ LargestNumberInArray.js
@@ -0,0 +1,21 @@
+// Нахождение массива с наибольшим числом.
+
+function largestOfFour(arr) {
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = 0;
+    for (var sb = 0; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+
+    results[n] = largestNumber;
+  }
+
+  return results;
+}
+
+
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
