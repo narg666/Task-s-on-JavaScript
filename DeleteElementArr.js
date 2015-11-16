53c3aa22b6cc1270b8a2cecf50c07e20f5c632b7
Valera Lyubavskiy <javanarg666@gmail.com>
2015-11-16 03:48:16 +0300

    Add Delete Element in Array

---
+++ DeleteElementArr.js
@@ -0,0 +1,10 @@
+// Удаление элементов из массива. 
+
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  args.splice(0, 1);
+  return arr.filter(function(element) {
+    return args.indexOf(element) === -1;
+  });
+}
+destroyer([1, 2, 3, 1, 2, 3], 2, 3); // [1, 1].
